cf6d39852165cd666083f270c0712349c1d37d2b
dougrchamberlain <d.chamberlain@nextech.com>
2021-05-26 23:22:55 -0400

    3 files modified, 4 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
 import { AppRootComponent } from './app-root/app-root.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, AppRootComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap: [AppRootComponent]
+  declarations: [AppRootComponent],
+  imports: [BrowserModule, FormsModule],
 })
-export class AppModule { }
+export class AppModule {}
